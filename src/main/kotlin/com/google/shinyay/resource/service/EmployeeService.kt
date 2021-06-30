c35e5c52bc76f913297bff51256adae3dcbd8523
shinyay <shinya.com@gmail.com>
2021-07-01 07:51:26 +0900

    Add service layer function

--- src/main/kotlin/com/google/shinyay/resource/service/EmployeeService.kt
+++ src/main/kotlin/com/google/shinyay/resource/service/EmployeeService.kt
@@ -1,5 +1,6 @@
 package com.google.shinyay.resource.service
 
+import com.google.shinyay.resource.model.Employee
 import com.google.shinyay.resource.repository.EmployeeRepository
 import org.springframework.stereotype.Service
 
@@ -7,4 +8,8 @@ import org.springframework.stereotype.Service
 class EmployeeService(val repository: EmployeeRepository) {
 
     fun findEmployeeById(id: Long) = repository.findById(id)
+
+    fun saveEmployee(employee: Employee) = repository.save(employee)
+
+    fun removeEmployee(id: Long) = repository.deleteById(id)
 }
