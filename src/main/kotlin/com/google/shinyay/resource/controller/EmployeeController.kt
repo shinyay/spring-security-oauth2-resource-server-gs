538d7bcac9244c83c93f6116060afa2f0a7c9877
shinyay <shinya.com@gmail.com>
2021-07-01 22:40:01 +0900

    Add find empmployee function with path variable

--- src/main/kotlin/com/google/shinyay/resource/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/resource/controller/EmployeeController.kt
@@ -1,10 +1,17 @@
 package com.google.shinyay.resource.controller
 
+import com.google.shinyay.resource.exception.EmployeeNotFoundException
+import com.google.shinyay.resource.model.Employee
+import com.google.shinyay.resource.service.EmployeeService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
-class EmployeeController {
-    
+class EmployeeController(val service: EmployeeService) {
+
+    @GetMapping("/employee/{id}")
+    fun findOne(@PathVariable id: Long): Employee = service.findEmployeeById(id).orElseThrow { EmployeeNotFoundException(id) }
 }
