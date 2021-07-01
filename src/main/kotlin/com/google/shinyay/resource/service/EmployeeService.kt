aafb0a0db39b39fd4688346a0a4ffc92cda6ca83
shinyay <shinya.com@gmail.com>
2021-07-01 22:04:19 +0900

    Add info log

--- src/main/kotlin/com/google/shinyay/resource/service/EmployeeService.kt
+++ src/main/kotlin/com/google/shinyay/resource/service/EmployeeService.kt
@@ -1,13 +1,18 @@
 package com.google.shinyay.resource.service
 
+import com.google.shinyay.resource.logger
 import com.google.shinyay.resource.model.Employee
 import com.google.shinyay.resource.repository.EmployeeRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class EmployeeService(val repository: EmployeeRepository) {
 
-    fun findEmployeeById(id: Long) = repository.findById(id)
+    fun findEmployeeById(id: Long): Optional<Employee> {
+        logger.info("Employee ID: $id")
+        return repository.findById(id)
+    }
 
     fun saveEmployee(employee: Employee) = repository.save(employee)
 
