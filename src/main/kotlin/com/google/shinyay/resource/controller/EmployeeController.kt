7aae7c1d60d2c2e1e3bfaf62b879877a67086d43
shinyay <shinya.com@gmail.com>
2021-07-02 08:22:35 +0900

    Add find register employees function

--- src/main/kotlin/com/google/shinyay/resource/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/resource/controller/EmployeeController.kt
@@ -3,10 +3,8 @@ package com.google.shinyay.resource.controller
 import com.google.shinyay.resource.exception.EmployeeNotFoundException
 import com.google.shinyay.resource.model.Employee
 import com.google.shinyay.resource.service.EmployeeService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,4 +15,8 @@ class EmployeeController(val service: EmployeeService) {
 
     @GetMapping("/employees/{id}")
     fun findOne(@PathVariable id: Long): Employee = service.findEmployeeById(id).orElseThrow { EmployeeNotFoundException(id) }
+
+    @PostMapping("/employees")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerEmployee(employee: Employee) = service.saveEmployee(employee)
 }
