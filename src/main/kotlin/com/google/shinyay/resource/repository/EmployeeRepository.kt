8e77d7a6c9b5702f4217cb3ae266834b49360562
shinyay <shinya.com@gmail.com>
2021-07-02 14:28:34 +0900

    Modify interface as JPA Repository

--- src/main/kotlin/com/google/shinyay/resource/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/resource/repository/EmployeeRepository.kt
@@ -1,6 +1,8 @@
 package com.google.shinyay.resource.repository
 
 import com.google.shinyay.resource.model.Employee
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 
-interface EmployeeRepository : PagingAndSortingRepository<Employee, Long>
+//interface EmployeeRepository : PagingAndSortingRepository<Employee, Long>
+interface EmployeeRepository : JpaRepository<Employee, Long>
