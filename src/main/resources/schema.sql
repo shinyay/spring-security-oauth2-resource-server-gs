861b5e9e20958f769a0fcecbaf168247a1aa139e
shinyay <shinya.com@gmail.com>
2021-07-02 14:26:56 +0900

    Add incremental definition

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    `id` bigint(20) NOT NULL,
+    `id` bigint(20) PRIMARY KEY AUTO_INCREMENT,
     `name` varchar(50) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
