e9e7f12e466b5199f0cf47d029418d5f9dd34fc2
shinyay <shinya.com@gmail.com>
2021-07-05 22:11:18 +0900

    Delete table annotation

--- src/main/kotlin/com/google/shinyay/resource/model/Employee.kt
+++ src/main/kotlin/com/google/shinyay/resource/model/Employee.kt
@@ -3,10 +3,9 @@ package com.google.shinyay.resource.model
 import javax.persistence.*
 
 @Entity
-@Table(name = "employee")
 data class Employee(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
-    val name: String
+    var name: String
 )
