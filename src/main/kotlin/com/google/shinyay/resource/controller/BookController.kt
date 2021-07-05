bde80060dbba7ccd41545735ca45a3613df1bb4f
shinyay <shinya.com@gmail.com>
2021-07-05 22:15:14 +0900

    Add book register function with POST

--- src/main/kotlin/com/google/shinyay/resource/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/resource/controller/BookController.kt
@@ -4,9 +4,7 @@ import com.google.shinyay.resource.model.Book
 import com.google.shinyay.resource.repository.BookRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,5 +21,10 @@ class BookController(val repository: BookRepository) {
         }
         return ResponseEntity(books, HttpStatus.OK)
     }
-    
+
+    @PostMapping("/books")
+    fun addNewBook(@RequestBody book: Book): ResponseEntity<Book> {
+        return ResponseEntity(repository.save(book), HttpStatus.OK)
+    }
+
 }
