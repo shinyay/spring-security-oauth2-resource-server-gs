9516e4776d2b18759a274e0adfd0a79e6a7de91d
shinyay <shinya.com@gmail.com>
2021-07-06 07:42:32 +0900

    Add authorization configuration

--- src/main/kotlin/com/google/shinyay/resource/config/SecurityConfig.kt
+++ src/main/kotlin/com/google/shinyay/resource/config/SecurityConfig.kt
@@ -1,7 +1,21 @@
 package com.google.shinyay.resource.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        http
+            ?.cors()
+            ?.and()
+            ?.authorizeRequests()
+            ?.antMatchers(HttpMethod.GET, "/api/v1/employees/**")?.hasAuthority("SCOPE_read")
+            ?.antMatchers(HttpMethod.POST, "/api/v1/employees")?.hasAuthority("SCOPE_write")
+            ?.anyRequest()?.authenticated()
+            ?.and()
+            ?.oauth2ResourceServer()?.jwt()
+    }
 }
