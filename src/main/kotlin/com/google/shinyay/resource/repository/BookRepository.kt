b7253f4530199e7bf5cad88f22014a946d3a4ea7
shinyay <shinya.com@gmail.com>
2021-07-05 22:23:38 +0900

    Delete Book operations

--- src/main/kotlin/com/google/shinyay/resource/repository/BookRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.google.shinyay.resource.repository
-
-import com.google.shinyay.resource.model.Book
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BookRepository : JpaRepository<Book, Long> {
-}
