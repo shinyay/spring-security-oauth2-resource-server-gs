5bc4190ca5ff8621b71e491bac82627b7190d8b5
shinyay <shinya.com@gmail.com>
2021-07-13 11:02:11 +0900

    Modify Security Configuration as EnableWebSecurity

--- src/main/kotlin/com/google/shinyay/resource/config/SecurityConfig.kt
+++ src/main/kotlin/com/google/shinyay/resource/config/SecurityConfig.kt
@@ -1,11 +1,11 @@
 package com.google.shinyay.resource.config
 
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-@Configuration
+@EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
         http
